1269e481c8eda53918cfc8827026fe0220e132a1
Dimpho Ramodise <dimpho.ramodise@gmail.com>
2019-08-02 16:07:14 +0200

    node modules

--- visitor.js
+++ visitor.js
@@ -1,32 +1,21 @@
-let id = 1;
-
-http.createServer(function(req, res){
-     res.writeHead(200, {'Content-Type': 'text/html'})
-     res.write('server is running');
-     res.end();
- 
-}).listen(port)
+let id = 0;
 
 
 class Visitor {
      constructor(fullName, age, visitDate, visitTime, comments, assistedBy) {
-          console.log('New patient file has been created');
+
           this.fullName = fullName;
           this.age = age;
           this.visitDate = visitDate;
           this.visitTime = visitTime;
           this.comments = comments;
           this.assistedBy = assistedBy;
-
-     }
-     counter(id) {
-          while (id) {
-               return id++
-          }
+          console.log('Patient information added to file');
+          this.id = id;
      }
 
      save() {
-          let count = this.counter;
+
           let files = new Visitor(
                this.fullName,
                this.age,
@@ -37,20 +26,28 @@ class Visitor {
           );
           let fileData = JSON.stringify(files)
           let fs = require('fs');
+          
+          id++;
 
-
-          fs.writeFile(`visitor_${count}.json`, fileData, err => {
+          fs.writeFile(`visitor_ ${id}.json`, fileData, err => {
                if (err) {
                     throw (Error + 'Cannot save file');
                } else {
                     console.log('File was saved');
                }
           });
+
+
      }
-     load(loadVisitor){
-           loadVisitor = this.save
-          return loadVisitor;
-     }
+     load(id) {
+          var fs = require("fs")
+          if(id <= 0) {
+              console.log("File doesn't exist");
+          }
+          var fileID = "visitor_" + id + ".json";
+          var visitor =  fs.readFileSync(fileID);
+          let visitorData = JSON.parse(visitor);
+      }
 }
 
 let visitor = new Visitor("Dee Ram",
@@ -60,3 +57,26 @@ let visitor = new Visitor("Dee Ram",
      "Persistent Cough",
      "Sister B");
 
+let visitor1 = new Visitor(
+     "Ram Gee",
+     "23",
+     "20 / 07 /19",
+     "09h56",
+     "Persistent cough",
+     "Sister B")
+
+let visitor2 = new Visitor(
+     "Jay K",
+     "23",
+     "20 / 07 /19",
+     "09h56",
+     "Persistent cough",
+     "Sister B")
+
+/* visitor.save();
+visitor1.save();
+visitor2.save(); */
+
+visitor.load(1);
+/* visitor1.load(2);
+visitor2.load(3); */

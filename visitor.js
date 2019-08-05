032706537e43a5cc0f0bd550240bee72976b8bd7
Dimpho Ramodise <dimpho.ramodise@gmail.com>
2019-08-05 11:12:18 +0200

    complete visitor class with load and save method

--- visitor.js
+++ visitor.js
@@ -1,6 +1,7 @@
+//  file ID number
 let id = 0;
 
-
+// class that contains the object visitor and its properties
 module.exports = class Visitor {
      constructor(fullName, age, visitDate, visitTime, comments, assistedBy) {
 
@@ -11,9 +12,8 @@ module.exports = class Visitor {
           this.comments = comments;
           this.assistedBy = assistedBy;
           console.log('Patient information added to file');
-          this.id = id;
      }
-
+// method save that saves patient information into a json file and assigns it an id
      save() {
 
           let files = new Visitor(
@@ -26,25 +26,30 @@ module.exports = class Visitor {
           );
           let fileData = JSON.stringify(files)
           const fs = require('fs');
-          
+
           id++;
 
-          fs.writeFile(`visitor_ ${id}.json`, fileData, err => {
+          fs.writeFile(`visitor${id}.json`, fileData, err => {
                if (err) {
                     throw (Error + 'Cannot save file');
-               }               
+               } else {
+                    console.log('File was saved');
+
+               }
           });
 
           return 'File was saved';
      }
+//   method load that reads the stored .son files and generates the file given the file number 
      load(id) {
           const fs = require("fs")
-          if(id <= 0) {
-              console.log("File doesn't exist");
+          if (id <= 0) {
+               console.log("File doesn't exist");
           }
-          var fileID = "visitor_" + id + ".json";
-          var visitor =  fs.readFileSync(fileID);
+          var fileID = `visitor${id}.json`;
+          var visitor = fs.readFileSync(fileID);
           let visitorData = JSON.parse(visitor);
-      }
-}
 
+          return visitorData;
+     }
+}
